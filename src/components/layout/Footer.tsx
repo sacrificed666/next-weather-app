7a51296dd46ab77d67e1e4ee6e06edb4df0bef4a
Illia Movchko <illia.movchko.concept@gmail.com>
2026-03-05 07:02:25 +0000

    style: shadow update

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="base w-full border-t">
       <div className="max-w-7xl w-full p-4 md:p-5 flex justify-between items-center">
-        <span>© 2025 Illia Movchko</span>
+        <span>© {new Date().getFullYear()} Illia Movchko</span>
         <SocialList>
           <SocialItem link="https://github.com/sacrificed666" icon="github" alt="GitHub" />
           <SocialItem link="https://www.linkedin.com/in/illiamovchko/" icon="linkedin" alt="LinkedIn" />
